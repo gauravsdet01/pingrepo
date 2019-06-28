598829785d600ce18c1d90f425fec2ebcc59810c
gaura <gaura@LAPTOP-SJ8DN68H>
2019-06-28 14:29:53 +0530

    ds

--- TLC Ping Test/src/test/java/automation/session/WebDriverFactory.java
+++ TLC Ping Test/src/test/java/automation/session/WebDriverFactory.java
@@ -83,10 +83,10 @@ public class WebDriverFactory {
 		ChromeOptions options = new ChromeOptions();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		
-		options.addArguments("test-type");
+		//options.addArguments("test-type");
 		//options.addArguments("--start-fullscreen");
-		options.addArguments("--disable-extensions");
-		options.addArguments("--start-maximized");
+		//options.addArguments("--disable-extensions");
+		//options.addArguments("--start-maximized");
 		options.addArguments("--headless");
 		options.addArguments("--disable-gpu");
 		options.addArguments("--ignore-certificate-errors");
